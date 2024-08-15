5da8bb1ca3cb819206bca119e7a73e4bae0a9790
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-08-15 12:05:48 -0700

    setting up test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
 #include <raylib.h>
 
+typedef enum {
+    HEART,
+    DIAMOND,
+    SPADE,
+    CLUB
+} Suit;
+
+class Card 
+{
+    public:
+    int num;
+    Suit suit;
+
+    Card(int num, Suit suit) {
+        this.num = num;
+        this.suit = suit;
+    }
+}
+
 const int window_size = 500;
 
 int main(int argc, char** argv) {
