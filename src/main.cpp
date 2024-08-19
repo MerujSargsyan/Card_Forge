d49e410c19a54901ce5787de59c2bee0a5dfc7d6
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-08-18 23:55:53 -0700

    game classes added

--- src/main.cpp
+++ src/main.cpp
@@ -24,19 +24,6 @@ typedef enum {
     DRAW4
 } CardType;
 
-class Game
-{
-public:
-    //Game(int player_count) {}
-    static void set_card_size(float x, float y) {
-        card_size = (Vector2){.x = x, .y = y};
-    }
-    void draw_cards()
-private:
-    Vector2 card_size;
-    std::vector<Container> containers;
-}
-
 class Card 
 {
 public:
@@ -75,38 +62,60 @@ private:
     Texture2D txt;
 };
 
-auto cards = std::vector<std::shared_ptr<Card>>();
+class Game
+{
+public:
+    Game(float card_width, float card_height) {
+        card_size = (Vector2){.x = card_width, .y = card_height};
+        cards = std::vector<std::shared_ptr<Card>>();
+    }
 
-void addCard(string color, int num, CardType t) {
-    auto card_ptr = std::make_shared<Card>(color, num, t);
-    cards.push_back(card_ptr);
-}
+    void update() {
+        for(const std::shared_ptr<Card>& card : cards) {
+            card->display();
+        }
+    }
 
-void addCard(string color, int num, CardType t, float x, float y) {
-    auto card_ptr = std::make_shared<Card>(color, num, t, x, y);
-    cards.push_back(card_ptr);
-}
+    static void set_card_size(float x, float y) {
+        //card_size = (Vector2){.x = x, .y = y};
+    }
+
+    void update_card_size() {
+        for(const std::shared_ptr<Card>& card : cards) {
+            //card->texture = ;
+        }
+    }
+
+    void add_card(string color, int num, CardType t, float x, float y) {
+        auto card_ptr = std::make_shared<Card>(color, num, t, x, y);
+        cards.push_back(card_ptr);
+    }
+
+private:
+    Vector2 card_size;
+    std::vector<std::shared_ptr<Card>> cards;
+};
+
+
 
 void update() {
-    for(const std::shared_ptr<Card>& card : cards) {
-        card->display();
-    }
+    
 }
 
 int main(int argc, char** argv) {
     SetTargetFPS(30);
 
-    Game.set_card_size(50.0f, 50.0f);
+    Game g = Game(50.0f, 50.0f);
     InitWindow(window_size, window_size, "CardForge");
 
-    addCard("blue", '0', NORMAL); // card is going out of scope inside of here
-    addCard("yellow", '0', NORMAL, 5.0f, 5.0f);
-    addCard("green", '9', NORMAL, 35.0f, 40.0f);
+    g.add_card("yellow", '0', NORMAL, 5.0f, 5.0f);
+    g.add_card("green", '9', NORMAL, 35.0f, 40.0f);
+    g.add_card("green", '9', NORMAL, 125.0f, 125.0f);
 
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(WHITE);
-        update();
+        g.update();
         EndDrawing();
     }
     CloseWindow();
