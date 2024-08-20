8c65564e9b9d7bea8c8fa4faa9cc546816e6a3c0
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-08-20 10:46:58 -0700

    improved build process a little

--- test/uno_test/src/maketest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CC="g++"
-CFLAGS="-Wall -Wextra -std=c++11"
-FORGESRC="../../../cardforge/src"
-RAYDIR="../../../raylib/src"
-
-$CC $CFLAGS -o uno_test uno_test.cpp -L$FORGESRC -I$FORGESRC -lcardforge -L$RAYDIR -I$RAYDIR -lraylib -framework Cocoa -framework IOKit -framework OpenGL
