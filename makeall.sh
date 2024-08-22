da2273a5f3209602b4a05f058702c6b20631b743
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-08-22 13:00:56 -0700

    added raylib submodule

--- makeall.sh
+++ makeall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git submodule update --init --recursive
 
 cd ./raylib/src/ && make && cd -
 cd ./cardforge/src/ && ./makelib.sh
--- test/uno_test/maketest.sh
+++ test/uno_test/maketest.sh
@@ -5,5 +5,8 @@ CFLAGS="-Wall -Wextra -std=c++11"
 FORGESRC="../../cardforge/src"
 RAYDIR="../../raylib/src"
 BUILDIR="./build"
-
-$CC $CFLAGS -o $BUILDIR/uno_test src/uno_test.cpp -L$FORGESRC -I$FORGESRC -lcardforge -L$RAYDIR -I$RAYDIR -lraylib -framework Cocoa -framework IOKit -framework OpenGL
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    $CC $CFLAGS -o $BUILDIR/uno_test src/uno_test.cpp -L$FORGESRC -I$FORGESRC -lcardforge -L$RAYDIR -I$RAYDIR -lraylib -framework Cocoa -framework IOKit -framework OpenGL
+else
+    $CC $CFLAGS -o $BUILDIR/uno_test src/uno_test.cpp -L$FORGESRC -I$FORGESRC -lcardforge -L$RAYDIR -I$RAYDIR -lraylib
+fi
