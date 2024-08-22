01b608d02aa78c3a3d2911549a40d1108debc3da
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-08-22 13:03:55 -0700

    build directory being built

--- test/uno_test/maketest.sh
+++ test/uno_test/maketest.sh
@@ -5,6 +5,9 @@ CFLAGS="-Wall -Wextra -std=c++11"
 FORGESRC="../../cardforge/src"
 RAYDIR="../../raylib/src"
 BUILDIR="./build"
+
+mkdir -p build
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
     $CC $CFLAGS -o $BUILDIR/uno_test src/uno_test.cpp -L$FORGESRC -I$FORGESRC -lcardforge -L$RAYDIR -I$RAYDIR -lraylib -framework Cocoa -framework IOKit -framework OpenGL
 else
