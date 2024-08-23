9269238a4f2271e9f875047165ee32301f79495e
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-08-23 09:58:54 -0700

    card spacing and scale added

--- cardforge/src/cardforge.cpp
+++ cardforge/src/cardforge.cpp
@@ -17,15 +17,15 @@ Card::Card(string color, char num, CardType type) {
 }
 
 void Card::display(float x, float y) {
-    if(IsTextureReady(txt)) DrawTextureEx(txt, (Vector2){x, y}, 0.0f, 0.5f, WHITE);
+    if(IsTextureReady(txt)) DrawTextureEx(txt, (Vector2){x, y}, 0.0f, card_scale, WHITE);
 }
 
 Card::~Card() {
     UnloadTexture(txt);
 }
 
-Game::Game(float card_scale) {
-    this->card_scale = card_scale;
+Game::Game(float scale) {
+    card_scale = scale;
     containers = std::vector<std::shared_ptr<Container>>();
 }
 
@@ -40,9 +40,21 @@ void Game::add_container(float start_x, float start_y, int init_count) {
     containers.push_back(cont_ptr);
 }
 
+void Game::add_container(float start_x, float start_y, float x_padding, int init_count) {
+    auto cont_ptr = std::make_shared<Container>(start_x, start_y, x_padding, init_count);
+    containers.push_back(cont_ptr);
+}
+
 Container::Container(float start_x, float start_y, int init_count) {
-    rect = (Rectangle){start_x, start_y, card_size.x * init_count, card_size.y};   
+    rect = (Rectangle){start_x, start_y, 1.0f, 1.0f};   
     cards = std::vector<std::shared_ptr<Card>>();
+    x_padding = 50.0f;
+}
+
+Container::Container(float start_x, float start_y, float x_padding, int init_count) {
+    rect = (Rectangle){start_x, start_y, 1.0f, 1.0f};   
+    cards = std::vector<std::shared_ptr<Card>>();
+    this->x_padding = x_padding;
 }
 
 void Container::add_card(string color, int num, CardType t) {
@@ -55,6 +67,6 @@ void Container::display() {
     float current_y = rect.y;
     for(const std::shared_ptr<Card>& card : cards) {
         card->display(current_x, current_y);
-        current_x += card_size.x;
+        current_x += x_padding;
     }
 }
--- test/uno_test/src/uno_test.cpp
+++ test/uno_test/src/uno_test.cpp
@@ -1,13 +1,19 @@
 #include <cardforge.hpp>
 
 int main(void) {
-    Game g = Game(75.0f, 50.0f);
+    Game g = Game(0.5f);
     InitWindow(500, 500, "Uno Test");
 
-    g.add_container(150.0f, 150.0f, 1);
+    g.add_container(150.0f, 150.0f, 25.0f, 1);
+    g.add_container(0.0f, 0.0f, 1);
+
     g.containers.at(0)->add_card("yellow", '0', NORMAL);
     g.containers.at(0)->add_card("green", '1', NORMAL);
     g.containers.at(0)->add_card("blue", '2', NORMAL);
+
+    g.containers.at(1)->add_card("yellow", '7', NORMAL);
+    g.containers.at(1)->add_card("green", '7', NORMAL);
+    g.containers.at(1)->add_card("blue", '7', NORMAL);
 
     while(!WindowShouldClose()) {
         BeginDrawing();
